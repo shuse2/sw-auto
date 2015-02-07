bcd6ce12d92e44d9a71187656bd8a442e7b6d4c0
shuse2 <shus.toda@gmail.com>
2015-02-07 11:31:35 +0900

    init commit

---
+++ sw_auto.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+while true
+do
+  adb shell input tap 1637 790
+  echo "tapped start"
+  sleep 1
+  adb shell input tap 800 740
+  echo "tapped center"
+  sleep 1
+  adb shell input tap 1150 890
+  echo "tap last"
+  sleep 1
+  adb shell input tap 1000 830
+  echo "tap item"
+  sleep 1
+  adb shell input tap 700 550
+  echo "tap again"
+  sleep 1
+done
